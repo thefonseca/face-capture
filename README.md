d2fe21b671af87b6ea5d7bb4c94fb9ca2ded0c30
Marcio Fonseca <thefonseca@gmail.com>
2019-01-11 00:36:36 -0200

    Option to process videos in a Youtube playlist.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # face-capture
+
+### Setup
+
+* Install the [Miniconda Python 3.x](https://conda.io/miniconda.html) version corresponding to the development 
+operating system.
+* Clone this repository:
+```
+git clone https://github.com/thefonseca/face-capture.git
+cd face-capture
+```
+
+* Create the conda environment (this should be done only once):
+```
+conda env create -f environment.yml
+```
+* Activate the conda environment:
+```
+source activate face-capture
+```
+
+### Detect faces in camera frames
+```
+python detect_faces_camera.py -p models/deploy.prototxt -m models/res10_300x300_ssd_iter_140000.caffemodel
+```
+
+### Detect faces in Youtube video
+```
+python detect_faces_youtube.py -t models/deploy.prototxt -m models/res10_300x300_ssd_iter_140000.caffemodel -v cUw18IAkmvE
+```
+
+### Detect and save faces in videos of a Youtube playlist
+```
+python detect_faces_youtube.py -t models/deploy.prototxt -m models/res10_300x300_ssd_iter_140000.caffemodel -p PLitz1J-q25kMvoRT9AIPfWoFIv4wzoUPi --playlist-start 1 -s 20
+```
--- detect_faces_youtube.py
+++ detect_faces_youtube.py
@@ -5,61 +5,84 @@ import time
 import facedetect
 import re
 import os
+from tqdm import tqdm
 
 URL_PATTERN = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required=True,
-                help="The youtube ID or URL")
-ap.add_argument("-p", "--prototxt", required=True,
-                help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-                help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-                help="minimum probability to filter weak detections")
-ap.add_argument("-s", "--save-every", type=int, default=0,
-                help="save detected faces to files every N frames. If zero, do not save face images.")
-ap.add_argument("-d", "--data-dir", type=str, default='data',
-                help="directory to save faces")
-args = vars(ap.parse_args())
 
-face_detector = facedetect.FaceDetector(args["prototxt"], args["model"])
+def detect_faces(face_detector, video, data_dir='data', save_every=0):
+    play = video.getbest()
+    # print(f"[INFO] starting video stream... '{video.title}'")
+    cap = cv2.VideoCapture(play.url)
+    time.sleep(1.0)
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-video = args['video']
+    frame_count = 0
+    video_faces_dir = os.path.join(data_dir, video.title)
 
-if not re.match(URL_PATTERN, video):
-    url = 'https://www.youtube.com/watch?v=' + video
+    if save_every > 0:
+        os.makedirs(video_faces_dir, exist_ok=True)
 
-video = pafy.new(video)
-play = video.getbest()
-cap = cv2.VideoCapture(play.url)
-time.sleep(2.0)
+    while True:
+        ret, frame = cap.read()
 
-frame_count = 0
-video_faces_dir = os.path.join(args['data_dir'], video.title)
+        if frame is None:
+            # print("[INFO] ending video stream...")
+            break
 
-if args['save_every'] > 0:
-    os.makedirs(video_faces_dir, exist_ok=True)
+        frame_count += 1
+        faces, frame = face_detector.detect(frame)
 
-while True:
-    ret, frame = cap.read()
+        if args['save_every'] > 0 and frame_count % save_every == 0:
+            for ii, face in enumerate(faces):
+                cv2.imwrite(os.path.join(video_faces_dir, f'frame_{frame_count}__face_{ii}.jpg'), face['image'])
 
-    if frame is None:
-        print("[INFO] ending video stream...")
-        break
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
-    frame_count += 1
-    faces, frame = face_detector.detect(frame)
+    cap.release()
 
-    if args['save_every'] > 0 and frame_count % args['save_every'] == 0:
-        for ii, face in enumerate(faces):
-            cv2.imwrite(os.path.join(video_faces_dir, f'frame_{frame_count}__face_{ii}.jpg'), face['image'])
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    group = ap.add_mutually_exclusive_group(required=True)
+    group.add_argument("-v", "--video", help="A youtube ID or URL")
+    group.add_argument("-p", "--playlist", help="A youtube playlist ID or URL")
+    ap.add_argument("--playlist-start", type=int, default=0,
+                    help="start index for videos in a playlist")
+    ap.add_argument("--playlist-end", type=int,
+                    help="end index for videos in a playlist")
+    ap.add_argument("-t", "--prototxt", required=True,
+                    help="path to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to Caffe pre-trained model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                    help="minimum probability to filter weak detections")
+    ap.add_argument("-s", "--save-every", type=int, default=0,
+                    help="save detected faces to files every N frames. If zero, do not save face images.")
+    ap.add_argument("-d", "--data-dir", type=str, default='data',
+                    help="directory to save faces")
+    args = vars(ap.parse_args())
 
-cap.release()
-cv2.destroyAllWindows()
+    detector = facedetect.FaceDetector(args["prototxt"], args["model"])
+
+    p_url = args.get('playlist')
+    if p_url:
+        if not re.match(URL_PATTERN, args['playlist']):
+            p_url = 'https://www.youtube.com/playlist?list=' + args['playlist']
+            playlist = pafy.get_playlist(p_url)
+
+            pbar = tqdm(playlist['items'][args['playlist_start']:args.get('playlist_end')])
+
+            for video in pbar:
+                video_title = video["pafy"].title
+                pbar.set_description(f'Processing video "{video_title}"')
+                detect_faces(detector, video['pafy'], data_dir=args['data_dir'], save_every=args['save_every'])
+    else:
+        v_url = args['video']
+        if not re.match(URL_PATTERN, v_url):
+            v_url = 'https://www.youtube.com/watch?v=' + v_url
+        video = pafy.new(v_url)
+        detect_faces(detector, video, data_dir=args['data_dir'], save_every=args['save_every'])
+
+    cv2.destroyAllWindows()
